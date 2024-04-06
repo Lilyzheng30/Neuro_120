68d34b7fb96f1f0b3e900c72c196280ff14ec933
Lilyzheng30 <144749803+Lilyzheng30@users.noreply.github.com>
2024-04-06 01:09:59 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,2 @@
 print("hi mira how are you doing. if you get this message it means i got ittttt")
+print("i also got it to pull correctly so now can you try to make change something and try to commit")
