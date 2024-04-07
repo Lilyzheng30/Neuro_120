7a76966ab476b381609bd7fe3c6c83bc6b650ca3
Lilyzheng30 <144749803+Lilyzheng30@users.noreply.github.com>
2024-04-07 16:22:02 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
 # made changes here i finally got it so you have to open the file then make changes and then save them on ur local computer file then push it to github
-# cloning is only when you very first start
+# cloning is only when you very first start and when you want to pull
 # network.py
 import numpy as np
 from matplotlib import pyplot as plt
